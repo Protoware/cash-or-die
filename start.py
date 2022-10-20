6f8d7534714b31857b3bec2c74414898e5d14138
Ivan <84027033+Tuxuis@users.noreply.github.com>
2022-10-20 21:45:57 +0200

    Update start.py
    First step into making game

--- start.py
+++ start.py
@@ -1,1 +1,34 @@
+# libraries
+
+from sys import exit
+from os import name, system
+from colorama import Fore, init
+init()
+
+# custom libraries
+
+from core.home.core_h import home
+
+
+# functions
+
+def clear():
+    if name == "nt":
+        s = system("cls")
+    else:
+        s = system("clear")
+def reset_c():
+    print(Fore.RESET)
+
+
+# main
+
+def main():
+    print("Welcome, good sir. What is your name?" + Fore.LIGHTBLUE_EX)
+    name = input("$ ")
+    reset_c()
+
+
+if __name__ == "__main__":
+    main()
 
