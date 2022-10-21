c722d8bfaa341f8cc847890f5a22c94b743aabd6
Ivan <84027033+Tuxuis@users.noreply.github.com>
2022-10-21 13:33:52 +0200

    Update core_h.py
    Done. More improvement may be added later.

--- core/home/core_h.py
+++ core/home/core_h.py
@@ -6,12 +6,17 @@ def home(name: str, tokens: int, bread: int, cash: int, storage: int) -> str:
     Welcome, {}
     """.format(name) + Fore.RESET
 
-    main = Fore.YELLOW + "\n    Tokens: {}".format(tokens) + Fore.RESET + Fore.WHITE + "   Bread: {}".format(bread) + Fore.RESET + Fore.LIGHTGREEN_EX + "   Cash: {}/{}".format(cash, storage)
+    main = Fore.YELLOW + "Tokens: {}".format(tokens) + Fore.RESET + Fore.WHITE + "   Bread: {}".format(bread) + Fore.RESET + Fore.LIGHTGREEN_EX + "   Cash: {}/{}".format(cash, storage) + Fore.RESET
+    
+    option = """
 
-    final = top + main
+    Commands
+    1 - Info
+    2 - Shop
+    3 - Mark's Casino
+    4 - Belly's Store
+    """
+
+    final = top + main + option
     print(final)
-
-
-# Testing
-home("Ivan", 20, 30, 10, 300)
 
