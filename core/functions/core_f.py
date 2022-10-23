4dbec5fbaee21e0814ff41e9e37e0cd8f138d743
Ivan <84027033+Tuxuis@users.noreply.github.com>
2022-10-23 22:36:36 +0200

    Update core_f.py
    Added exit_check function

--- core/functions/core_f.py
+++ core/functions/core_f.py
@@ -1,17 +1,23 @@
 from os import name, system
-from colorama import Fore, init
+from colorama import Fore, init, Style
 
 def clear():
     if name == "nt":
         s = system("cls")
     else:
         s = system("clear")
+
 def reset_c():
     print(Fore.RESET)
 
 def get_input():
     i = input(Fore.LIGHTBLUE_EX + "$ ")
-    if i == "" or " ":
-        return "none"
-    else:
-        return i
+    return i
+
+def exit_check(inp):
+    if inp == "4":
+        print(Fore.YELLOW + "Are you sure..? y/n" + Fore.RESET)
+        c = get_input()
+        if c == "y":
+            print(Fore.WHITE + Style.BRIGHT + "Alright, goodbye!" + Fore.RESET + Style.RESET_ALL)
+            exit()
