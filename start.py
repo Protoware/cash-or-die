c3737d4affac6a94640c6c8bc5eea06b0b800e51
Ivan <84027033+Tuxuis@users.noreply.github.com>
2022-10-23 22:34:55 +0200

    Update start.py
    Now displays the menu. Still work in progress.

--- start.py
+++ start.py
@@ -5,30 +5,48 @@ from os import name, system
 from colorama import Fore, init
 init()
 
-# custom libraries
-
 from core.home.core_h import home
+from core.functions.core_f import clear, get_input, reset_c, exit_check
 
 
-# functions
 
-def clear():
-    if name == "nt":
-        s = system("cls")
-    else:
-        s = system("clear")
-def reset_c():
-    print(Fore.RESET)
+# Data
+
+storage  =   0
+tokens   =   0
+bread    =   0
+cash     =   0
 
 
 # main
 
-def main():
+def main(name: str):
+    global storage, tokens, bread, cash
+    clear()
+    home(name, tokens, bread, cash, storage)
+    i = get_input()
+    exit_check(i)
+
+    if i == "1":
+        return 0
+
+    main(name)
+
+
+# start
+
+def start():
+    global storage, tokens, bread, cash, name
+    storage = 300
+    cash = 50
+    clear()
     print("Welcome, good sir. What is your name?" + Fore.LIGHTBLUE_EX)
     name = input("$ ")
+    name = name
     reset_c()
 
+    main(name)
 
 if __name__ == "__main__":
-    main()
+    start()
 
